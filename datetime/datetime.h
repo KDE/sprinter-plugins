a5770712a8d3e058e7b8d96934d838acf32bc0c6
Aaron Seigo <aseigo@kde.org>
2013-12-31 18:33:20 +0100

    make it 2014 as i haven't pushed it anywhere yet.
    first project of the new year :)

--- datetime/datetime.h
+++ datetime/datetime.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Aaron Seigo <aseigo@kde.org>
+ * Copyright (C) 2014 Aaron Seigo <aseigo@kde.org>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
