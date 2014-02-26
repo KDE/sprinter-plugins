8739e6f54072d8793a88ef4ab2c5cba1ef06c52a
Aaron J. Seigo <aseigo@kde.org>
2014-02-26 16:20:59 +0100

    recent documents runner
    numerous improvements over the 4.x version including:
    
    * matching on the actual name of the file, not the .desktop
        (funny tip: enter "desktop" into krunner and you get all your recent docs!)
    * duplicate removal
    * use the url in the .desktop file (one less layer of indirection)
    * differentiate between exact/partial matching

---
+++ recentdocs/recentdocs.cpp
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2014 Aaron Seigo <aseigo@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "recentdocs.h"
+
+#include <QCoreApplication>
+#include <QDebug>
+#include <QEventLoop>
+#include <QIcon>
+
+#include <KConfig>
+#include <KDesktopFile>
+#include <KIOCore/KRecentDocument>
+#include <KIOWidgets/KRun>
+#include <KCoreAddons/KDirWatch>
+
+RecentDocsSessionData::RecentDocsSessionData(Sprinter::Runner *runner)
+    : Sprinter::RunnerSessionData(runner)
+{
+    updateRecentDocsList();
+
+    KDirWatch *watch = new KDirWatch(this);
+    watch->addDir(KRecentDocument::recentDocumentDirectory(),
+                  KDirWatch::WatchFiles);
+    connect(watch, &KDirWatch::created,
+            this, &RecentDocsSessionData::updateRecentDocsList);
+    connect(watch, &KDirWatch::deleted,
+            this, &RecentDocsSessionData::updateRecentDocsList);
+    connect(watch, &KDirWatch::dirty,
+            this, &RecentDocsSessionData::updateRecentDocsList);
+}
+
+void RecentDocsSessionData::updateRecentDocsList()
+{
+    recentDocs.clear();
+    QSet<QString> seen;
+    RecentDoc doc;
+
+    foreach (const QString desktopFile, KRecentDocument::recentDocuments()) {
+        KDesktopFile file(desktopFile);
+        doc.name = file.readName();
+        if (seen.contains(doc.name)) {
+            continue;
+        }
+
+        seen.insert(doc.name);
+        doc.icon = file.readIcon();
+        doc.url = file.readUrl();
+        recentDocs << doc;
+    }
+}
+
+RecentDocsRunner::RecentDocsRunner(QObject *parent)
+    : Sprinter::Runner(parent)
+{
+    setMatchTypesGenerated(QVector<Sprinter::QuerySession::MatchType>()
+                                << Sprinter::QuerySession::FileType);
+    setSourcesUsed(QVector<Sprinter::QuerySession::MatchSource>()
+                        << Sprinter::QuerySession::FromFilesystem);
+}
+
+Sprinter::RunnerSessionData *RecentDocsRunner::createSessionData()
+{
+    return new RecentDocsSessionData(this);
+}
+
+void RecentDocsRunner::match(Sprinter::MatchData &matchData)
+{
+    RecentDocsSessionData *sessionData = qobject_cast<RecentDocsSessionData *>(matchData.sessionData());
+    if (!sessionData || sessionData->recentDocs.isEmpty()) {
+        return;
+    }
+
+    const QString term = matchData.queryContext().query();
+    for (int i = 0; i < sessionData->recentDocs.count(); ++i) {
+        if (sessionData->recentDocs[i].name.contains(term, Qt::CaseInsensitive)) {
+            Sprinter::QueryMatch match;
+            match.setTitle(sessionData->recentDocs[i].name);
+            match.setText(tr("Recent Document"));
+            match.setType(Sprinter::QuerySession::FileType);
+            match.setSource(Sprinter::QuerySession::FromFilesystem);
+
+            if (sessionData->recentDocs[i].name.compare(term, Qt::CaseInsensitive) == 0) {
+                match.setPrecision(Sprinter::QuerySession::ExactMatch);
+            } else {
+                match.setPrecision(Sprinter::QuerySession::CloseMatch);
+            }
+
+            match.setImage(QIcon::fromTheme(sessionData->recentDocs[i].icon).pixmap(matchData.queryContext().imageSize()).toImage());
+            match.setUserData(sessionData->recentDocs[i].url);
+            match.setData(sessionData->recentDocs[i].url);
+            matchData << match;
+        }
+    }
+}
+
+bool RecentDocsRunner::exec(const Sprinter::QueryMatch &match)
+{
+    bool success = false;
+    QEventLoop loop;
+    KRun *krun = new KRun(match.data().toString(), 0, false);
+    connect(krun, &KRun::finished,
+            [&]() { success = !krun->hasError(); loop.exit(); });
+    krun->moveToThread(QCoreApplication::instance()->thread());
+    loop.exec();
+    return success;
+}
+
+#include "moc_recentdocs.cpp"
